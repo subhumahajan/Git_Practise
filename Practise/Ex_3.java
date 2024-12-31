08be62a688a71bee558ae56deb5c1473646590bc
subhu mahajan <subhumahajan96@gmail.com>
2024-12-31 13:08:10 +0530

    third commit

---
+++ Practise/Ex_3.java
@@ -0,0 +1,12 @@
+package Practise;
+
+public class Ex_3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my git testing practise");
+		System.out.println("First commit done");
+		System.out.println("Second commit done");
+	}
+
+}

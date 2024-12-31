29d34a6891621d44f25fc58a27ca8647434d2cd5
subhu mahajan <subhumahajan96@gmail.com>
2024-12-31 13:31:03 +0530

    fourth commit

---
+++ Practise/Ex_4.java
@@ -0,0 +1,13 @@
+package Practise;
+
+public class Ex_4 {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my git testing practise");
+		System.out.println("First commit done");
+		System.out.println("Second commit done");
+		System.out.println("TThird commit done");
+	}
+
+}
